7be33ebd662b7d39f57815b87dbbc6c2259d3ef6
yaakov_sahale <yaakov.sahale@gmail.com>
2021-08-21 21:11:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,92 @@
+
+
+//!.................................1
+
+// function getRandomNumber(){
+//     var num = Math.floor(Math.random()*100)
+//     return num
+// }
+
+// console.log(getRandomNumber())
+
+//!...................................2
+
+// function getRandomNumber(){
+//     var num = Math.floor(Math.random()*10000)
+//     return num
+// }
+
+// console.log(getRandomNumber())
+
+//!....................................3
+
+
+// function getRandomNumber(){
+//     var num = Math.floor(Math.random()*55)
+//     return num
+// }
+
+// console.log(getRandomNumber())
+
+//!.....................................4
+
+
+// function getRandomNumber(){
+//     var userNumber = Number(prompt("enter a number"))
+//     var num = Math.floor(Math.random()*userNumber)
+//     return num
+// }
+
+// console.log(getRandomNumber())
+
+//!.......................................5
+
+// function getRandomNumber(){
+//     var userNumber = Number(prompt("enter a number between 0-9"))
+//     var num = Math.floor(Math.random()*10)
+
+//     if(userNumber == num){
+//         return "your number is equle to the random number"    
+
+//     }
+//     return "your number isn't equle to the random number"    
+
+// }
+
+// console.log(getRandomNumber())
+
+//!........................................6
+
+// function getRandomNumber(){
+//     var userNumber = Number(prompt("enter a number between 0-99"))
+//     var num = Math.floor(Math.random()*100)
+
+//     if(userNumber == num){
+//         return "your number is equle to the random number"    
+
+//     }
+//     return "your number isn't equle to the random number"    
+
+// }
+
+// console.log(getRandomNumber())
+
+//!.........................................7
+
+// function getRandomNumber(userNumber, userName){
+//     var num = Math.floor(Math.random()*100)
+    
+//     if(userNumber == num){
+//         return `well done ${userName}, your number ${userNumber} is the 
+//         winner number`    
+        
+//     }
+//     return `better luck next time ${userName}, your number ${userNumber} isn't the 
+//     winner number`   
+    
+// }
+
+// var inputNumber = Number(prompt("enter a number between 0-9"))
+// var inputName = prompt("enter your name")
+
+// console.log(getRandomNumber(inputNumber, inputName))
